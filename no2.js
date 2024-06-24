d5d2ec1b4e39f9b0f47dc7f402e9e059cd77f955
mronim <roni.mhd@gmail.com>
2024-06-25 03:38:57 +0700

    no2FS

---
+++ no2.js
@@ -0,0 +1,18 @@
+let array = [20, 12, 35, 11, 17, 9, 58, 23, 69, 21];
+
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+let sortedArray = bubbleSort(array);
+console.log(sortedArray);
